7ec29fe48a59133e7e250b41b92eb0c466ac6667
Oliver Pirger <0x4f48@proton.me>
2025-02-27 18:49:04 +0100

    Sync again

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,79 @@
-use anyhow::bail;
-use clap::Parser;
-use futures_util::{SinkExt, StreamExt};
-use std::net::SocketAddr;
-use std::sync::Arc;
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
-use tokio::sync::broadcast::{Receiver, Sender};
-use tokio::sync::{broadcast, Mutex};
-use tokio_serial::{SerialPortBuilderExt, SerialStream};
-use tokio_tungstenite::accept_async;
-use tokio_tungstenite::tungstenite::Message;
-use tokio_util::sync::CancellationToken;
-use tracing::{debug, error, info, warn, Level};
+mod error;
 
-#[derive(clap::Parser)]
+use crate::error::{ConfigureWriterError, ParseLineError};
+use clap::Parser;
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufWriter, Write};
+use std::sync::atomic::Ordering;
+use tracing::{error, info, Level};
+
+#[derive(Parser)]
 struct Args {
     /// Serial port assigned to LoRa receiver
     port: String,
-
-    /// Print debug information
     #[arg(short, long)]
+    /// Print debug information
     debug: bool,
-
-    /// Define an output CSV file
-    #[arg(short, long, value_name = "DIRECTORY")]
+    #[arg(short, long, value_name = "FILE")]
+    /// Output CSV file
     output: Option<std::path::PathBuf>,
-
-    /// Launch WebSocket server for web panel
+    /// Launch WebSocket server
     #[arg(short, long, value_name = "ADDRESS")]
     websocket: Option<String>,
 }
 
-#[derive(Clone, Debug, serde::Serialize)]
-struct Data([isize; 23]);
+const TELEMETRY_LEN: usize = 28;
+type Telemetry = [String; TELEMETRY_LEN];
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), error::MainError> {
+    let running = std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true));
+    let running_clone = running.clone();
+    ctrlc::set_handler(move || running_clone.store(false, Ordering::SeqCst))?;
+
     let args = Args::parse();
+    configure_tracing(args.debug)?;
+    info!("Starting charter on {}...", args.port);
 
-    setup_tracing(args.debug)?;
-    let serial = Arc::new(Mutex::new(setup_serial(args.port)?));
+    let mut serial = initialize_serial(args.port)?;
+    serial.write_all(b"radio rxstop")?;
+    serial.write_all(b"radio rx 0")?;
+    let mut reader = std::io::BufReader::new(&mut serial).lines();
     let mut writer = match args.output {
-        Some(ref output) => Some(setup_writer(output).await?),
+        Some(path) => Some(configure_writer(path)?),
         None => None,
     };
 
-    serial
-        .clone()
-        .lock()
-        .await
-        .write_all(b"radio rx 0\r\n")
-        .await?;
-
-    let (websocket_tx, _websocket_rx) = broadcast::channel::<String>(1024);
-    let tx_clone = websocket_tx.clone();
-    if let Some(address) = args.websocket.clone() {
-        setup_websocket(&address, tx_clone).await?;
-    };
-
-    let serial_clone = serial.clone();
-    let token = CancellationToken::new();
-    let child_token = token.child_token();
-
-    let process = tokio::spawn(async move {
-        let mut serial = serial_clone.lock().await;
-        let mut reader = tokio::io::BufReader::new(&mut *serial).lines();
-        let mut index: usize = 0;
-        loop {
-            tokio::select! {
-                _ = child_token.cancelled() => break,
-                Ok(Some(line)) = reader.next_line() => {
-                    let data = match parse_line(line) {
-                Ok(data) => data,
+    while running.load(Ordering::SeqCst) {
+        let line = match reader.next() {
+            Some(line) => match line {
+                Ok(line) => line,
                 Err(error) => {
-                    warn!("{error}");
-                    continue;
-                }
-            };
-            if let Some(writer) = &mut writer {
-                if let Err(error) = write_csv(writer, &data).await {
                     error!("{error}");
                     continue;
                 }
-            };
-            if args.websocket.is_some() {
-                match serde_json::to_string(&data) {
-                    Ok(data) => {
-                        if let Err(error) = websocket_tx.send(data) {
-                            error!("{error}");
-                        }
-                    }
-                    Err(error) => error!("{error}"),
-                };
+            },
+            None => continue,
+        };
+        let data = match parse_line(line) {
+            Ok(data) => data,
+            Err(error) => {
+                error!("{error}");
+                continue;
             }
-            info!("{index} {data:?}");
-            index += 1;
-                }
-            }
+        };
+        if let Some(writer) = &mut writer {
+            writer.write_record(&data)?;
         }
-    });
-
-    tokio::select! {
-        _ = process => {},
-        _ = tokio::signal::ctrl_c() => {
-            info!("Shutting down...");
-            token.cancel();
-            serial
-                .clone()
-                .lock()
-                .await
-                .write_all(b"radio rxstop\r\n")
-                .await?;
-        }
+        info!("{:?}", data);
     }
+
+    info!("Stopping charter...");
+    serial.write_all(b"radio rxstop")?;
     Ok(())
 }
 
-/// Sets up a tracing subscriber and sets it as default for logging.
-/// A boolean is provided for enabling debug logging.
-fn setup_tracing(debug: bool) -> anyhow::Result<()> {
+fn configure_tracing(debug: bool) -> Result<(), tracing::dispatcher::SetGlobalDefaultError> {
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
         .with_max_level(if debug { Level::TRACE } else { Level::INFO })
         .finish();
@@ -126,111 +81,47 @@ fn setup_tracing(debug: bool) -> anyhow::Result<()> {
     Ok(())
 }
 
-/// Opens a serial connection on the provided serial port.
-fn setup_serial(port: String) -> anyhow::Result<SerialStream> {
-    let serial = tokio_serial::new(port, 115200)
-        .data_bits(tokio_serial::DataBits::Eight)
-        .parity(tokio_serial::Parity::None)
-        .stop_bits(tokio_serial::StopBits::One)
-        .open_native_async()?;
-    Ok(serial)
-}
-
-/// Creates a WebSocket server and starts accepting connections
-async fn setup_websocket(address: &String, tx: Sender<String>) -> anyhow::Result<()> {
-    let listener = tokio::net::TcpListener::bind(&address).await?;
-    info!("Websocket listening on ws://{}", address);
-    while let Ok((stream, _)) = listener.accept().await {
-        let peer = match stream.peer_addr() {
-            Ok(peer) => peer,
-            Err(error) => {
-                error!("{error}");
-                continue;
-            }
-        };
-        debug!("{} initiated connection to WebSocket server...", peer);
-        tokio::spawn(accept_connection(peer, stream, tx.subscribe()));
+fn configure_writer(
+    output: std::path::PathBuf,
+) -> Result<csv::Writer<BufWriter<File>>, ConfigureWriterError> {
+    if output.is_file() {
+        return Err(ConfigureWriterError::NotFile);
     }
-    Ok(())
+    fs::create_dir_all(&output)?;
+    let file = BufWriter::new(
+        fs::OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(output.join("primary.csv"))?,
+    );
+    Ok(csv::Writer::from_writer(file))
 }
 
-/// Accepts a WebSocket connection and starts sending messages to the client.
-async fn accept_connection(
-    peer: SocketAddr,
-    stream: TcpStream,
-    rx: Receiver<String>,
-) -> anyhow::Result<()> {
-    handle_connection(peer, stream, rx).await?;
-    Ok(())
+fn initialize_serial(port: String) -> Result<Box<dyn serialport::SerialPort>, std::io::Error> {
+    Ok(serialport::new(port, 115200)
+        .data_bits(serialport::DataBits::Eight)
+        .parity(serialport::Parity::None)
+        .stop_bits(serialport::StopBits::One)
+        .timeout(std::time::Duration::from_millis(3000))
+        .open()?)
 }
 
-/// Sends messages to the WebSocket client.
-async fn handle_connection(
-    peer: SocketAddr,
-    stream: TcpStream,
-    mut rx: Receiver<String>,
-) -> anyhow::Result<()> {
-    let ws_stream = accept_async(stream).await?;
-    debug!("{peer} connected via WebSocket");
-    let (mut ws_sender, _ws_receiver) = ws_stream.split();
-    tokio::spawn(async move {
-        loop {
-            match rx.recv().await {
-                Ok(data) => {
-                    ws_sender
-                        .send(Message::from(data))
-                        .await
-                        .map_err(|error| error!("{error}"))
-                        .unwrap();
-                }
-                Err(error) => error!("{error}"),
-            }
-        }
-    });
-    Ok(())
-}
-
-/// Creates a file writer for writing CSV data to the given file.
-async fn setup_writer(
-    file: &std::path::Path,
-) -> anyhow::Result<tokio::io::BufWriter<tokio::fs::File>> {
-    let writer = tokio::fs::OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(file)
-        .await?;
-    let writer = tokio::io::BufWriter::new(writer);
-    Ok(writer)
-}
-
-/// Parses a line from WLR089 LoRa module by extracting the message,
-/// converting the HEX to UTF-8, and splitting the data into an array of integers.
-fn parse_line(line: String) -> anyhow::Result<Data> {
+fn parse_line(line: String) -> Result<Telemetry, ParseLineError> {
     let parts: Vec<&str> = line.split_whitespace().collect();
     if parts.len() != 2 {
-        debug!("Unexpected line: {line}");
-        bail!("Unexpected line");
+        return Err(ParseLineError::UnexpectedLine(
+            "line does not contain a message or is corrupted",
+        ));
     }
-    let data_bytes = hex::decode(parts[1])?;
-    let data_str = String::from_utf8(data_bytes)?;
-
-    let mut data_array: [isize; 23] = [0; 23];
-    for (index, value) in data_str.split_whitespace().enumerate() {
-        data_array[index] = value.parse()?;
+    let data = hex::decode(parts[1])?;
+    let data = String::from_utf8(data)?;
+    let data = data.split_whitespace().collect::<Vec<&str>>();
+    let mut telemetry: Telemetry = [const { String::new() }; TELEMETRY_LEN];
+    if data.len() != TELEMETRY_LEN {
+        return Err(ParseLineError::InvalidDataFormat("unexpected data length"));
     }
-    Ok(Data(data_array))
-}
-
-/// Writes a line to the writer in CSV format.
-async fn write_csv(
-    writer: &mut tokio::io::BufWriter<tokio::fs::File>,
-    data: &Data,
-) -> anyhow::Result<()> {
-    let mut wtr = csv::WriterBuilder::new()
-        .has_headers(false)
-        .from_writer(vec![]);
-    wtr.serialize(data)?;
-    let bytes = wtr.into_inner()?;
-    writer.write_all(&bytes).await?;
-    Ok(())
+    for (index, value) in data.iter().enumerate() {
+        telemetry[index] = value.to_string();
+    }
+    Ok(telemetry)
 }
