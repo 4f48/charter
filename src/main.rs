b7179fa5b0c6dbcf2bea9f6ba0d477f23d8cb46d
Oliver Pirger <0x4f48@proton.me>
2025-02-27 19:35:36 +0100

    WebSocket

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod error;
 
-use crate::error::{ConfigureWriterError, ParseLineError};
+use crate::error::{ConfigureWriterError, MainError, ParseLineError};
 use clap::Parser;
 use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufWriter, Write};
-use std::sync::atomic::Ordering;
+use std::net::TcpStream;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread::spawn;
 use tracing::{error, info, Level};
+use tungstenite::{accept, WebSocket};
 
 #[derive(Parser)]
 struct Args {
@@ -19,15 +23,17 @@ struct Args {
     /// Output CSV file
     output: Option<std::path::PathBuf>,
     /// Launch WebSocket server
-    #[arg(short, long, value_name = "ADDRESS")]
+    #[arg(short, long, value_name = "PORT")]
     websocket: Option<String>,
 }
 
 const TELEMETRY_LEN: usize = 28;
 type Telemetry = [String; TELEMETRY_LEN];
 
-fn main() -> Result<(), error::MainError> {
-    let running = std::sync::Arc::new(std::sync::atomic::AtomicBool::new(true));
+type Websocket = Result<Option<Arc<Mutex<Vec<WebSocket<TcpStream>>>>>, MainError>;
+
+fn main() -> Result<(), MainError> {
+    let running = Arc::new(AtomicBool::new(true));
     let running_clone = running.clone();
     ctrlc::set_handler(move || running_clone.store(false, Ordering::SeqCst))?;
 
@@ -35,14 +41,17 @@ fn main() -> Result<(), error::MainError> {
     configure_tracing(args.debug)?;
     info!("Starting charter on {}...", args.port);
 
-    let mut serial = initialize_serial(args.port)?;
+    let mut serial = initialize_serial(&args.port)?;
     serial.write_all(b"radio rxstop")?;
     serial.write_all(b"radio rx 0")?;
     let mut reader = std::io::BufReader::new(&mut serial).lines();
+
     let mut writer = match args.output {
-        Some(path) => Some(configure_writer(path)?),
+        Some(ref path) => Some(configure_writer(path)?),
         None => None,
     };
+
+    let websocket = initialize_websocket(&running, &args)?;
 
     while running.load(Ordering::SeqCst) {
         let line = match reader.next() {
@@ -62,15 +71,82 @@ fn main() -> Result<(), error::MainError> {
                 continue;
             }
         };
+
         if let Some(writer) = &mut writer {
             writer.write_record(&data)?;
         }
+
+        if let Some(clients) = &websocket {
+            let json = match serde_json::to_string(&data) {
+                Ok(json) => json,
+                Err(error) => {
+                    error!("{error}");
+                    continue;
+                }
+            };
+            let mut clients = match clients.lock() {
+                Ok(clients) => clients,
+                Err(error) => {
+                    error!("{error}");
+                    continue;
+                }
+            };
+            clients.retain_mut(|websocket| {
+                match websocket.send(tungstenite::Message::Text(json.clone().into())) {
+                    Ok(_) => true,
+                    Err(error) => {
+                        error!("{error}");
+                        false
+                    }
+                }
+            });
+        }
+
         info!("{:?}", data);
     }
 
     info!("Stopping charter...");
     serial.write_all(b"radio rxstop")?;
     Ok(())
+}
+
+fn initialize_websocket(running: &Arc<AtomicBool>, args: &Args) -> Websocket {
+    let websocket = match &args.websocket {
+        Some(port) => {
+            let clients = Arc::new(Mutex::new(Vec::new()));
+            let websocket_clients = clients.clone();
+
+            let server = std::net::TcpListener::bind(format!("0.0.0.0:{port}"))?;
+            server.set_nonblocking(true)?;
+            info!("WebSocket server listening on 0.0.0.0:{port}...");
+
+            let running = running.clone();
+            spawn(move || {
+                while running.load(Ordering::SeqCst) {
+                    match server.accept() {
+                        Ok((stream, address)) => {
+                            let clients = clients.clone();
+                            spawn(move || match accept(stream) {
+                                Ok(stream) => {
+                                    let mut clients = clients.lock().unwrap();
+                                    clients.push(stream);
+                                    info!("New WebSocket connection from {address}");
+                                }
+                                Err(error) => error!("{error}"),
+                            });
+                        }
+                        Err(ref error) if error.kind() == std::io::ErrorKind::WouldBlock => {
+                            continue
+                        }
+                        Err(error) => error!("{error}"),
+                    }
+                }
+            });
+            Some(websocket_clients)
+        }
+        None => None,
+    };
+    Ok(websocket)
 }
 
 fn configure_tracing(debug: bool) -> Result<(), tracing::dispatcher::SetGlobalDefaultError> {
@@ -82,22 +158,21 @@ fn configure_tracing(debug: bool) -> Result<(), tracing::dispatcher::SetGlobalDe
 }
 
 fn configure_writer(
-    output: std::path::PathBuf,
+    output: &std::path::PathBuf,
 ) -> Result<csv::Writer<BufWriter<File>>, ConfigureWriterError> {
     if output.is_file() {
         return Err(ConfigureWriterError::NotFile);
     }
-    fs::create_dir_all(&output)?;
     let file = BufWriter::new(
         fs::OpenOptions::new()
             .append(true)
             .create(true)
-            .open(output.join("primary.csv"))?,
+            .open(output)?,
     );
     Ok(csv::Writer::from_writer(file))
 }
 
-fn initialize_serial(port: String) -> Result<Box<dyn serialport::SerialPort>, std::io::Error> {
+fn initialize_serial(port: &String) -> Result<Box<dyn serialport::SerialPort>, std::io::Error> {
     Ok(serialport::new(port, 115200)
         .data_bits(serialport::DataBits::Eight)
         .parity(serialport::Parity::None)
